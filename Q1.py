8f914c523600b360c58f58d36faab9504ef3e4a9
Limseunghyeon <sounghyn105@naver.com>
2021-05-08 21:20:23 +0900

    fdg

--- Q1.py
+++ Q1.py
@@ -1,39 +1,93 @@
 '''
-시작 시각 :
-종료 시각 :
+시작 시각 : 14시
+종료 시각 : 15시 30분
 '''
-
+import copy ##ㅠㅠ
 
 class Cake:
-    def __init__(self):
-        pass
+    def __init__(self,name,price,ingredient):
+        self.name=name
+        self.price=price
+        self.ingredient=ingredient
 
 
 class Shop:
     def __init__(self):
-        pass
+        self.ingredient=dict()
+        self.product=dict()
 
-    def buy_ingredient(self, buy_dict):
-        pass
+    def buy_ingredient(self, buy_dict): #buy ingredient_ 구매한 재료들
+        for key,val in buy_dict.items():
+            if key in self.ingredient:
+                self.ingredient[key]+=val
+            else: #when there's no matches
+                self.ingredient[key]=val
+
+
 
     def current_ingredient(self):
-        pass
+        print("현재 보유한 재료는 ",end='')
+        for key,val in self.ingredient.items():
+            print(f"{key}({val}개)",end=' ')
+        print('입니다.')
 
-    def make_cake(self, cake):
-        pass
+    def make_cake(self, cake): #cake(name,price,ingre)
+        error=dict()  #[모자란 재료: 개수]
+        cop=copy.deepcopy(self.ingredient)##error 날때를 위해 미리 복사
+        #print('cop:',cop)
+        for key,val in cake.ingredient.items():
+
+            if key not in self.ingredient: #재료가 없을때
+                error[key]=val #error에 추가
+            else:
+
+                if (self.ingredient[key]-val)<0:
+                    error[key]=-self.ingredient[key]+val
+                    del self.ingredient[key]
+                elif (self.ingredient[key]-val)==0:
+                    del self.ingredient[key]
+                else:
+                     self.ingredient[key]-=val
+
+        if error==dict(): #ingredient is enough
+            if cake in self.product:
+                self.product[cake]+=1
+            else:
+                self.product[cake]=1
+            print(cake.name,"1개 완성!")
+        else: #not enough ingredient
+            for key, val in error.items():
+                print(f"{key}재료가 {val}개",end=' ')
+            print("부족해요!")
+            self.ingredient=copy.deepcopy(cop)
 
 
 class Pos:
-    def __init__(self):
-        pass
+    def __init__(self,cake_shop): #cake_shop=shop의 객체
+        self.shop=cake_shop
+        self.money=0
+
 
     def current_cakes(self):
-        pass
+        print("현재 재고는")
+        for key,val in self.shop.product.items():
+            print(key.name,':',val)
 
     def sell_cake(self, cakename):
-        pass
+        for cake in self.shop.product: #product: cake class & 개수
+            if cake.name==cakename:
+                self.shop.product[cake]-=1
+                self.money+=cake.price #판매금액 더해주기
+                if self.shop.product[cake]<=0:
+                    del self.shop.product[cake]
+                print(cakename,"판매 완료. 현재 남은",cakename,"개수는",self.shop.product.get(cake,0),"입니다.")
+
+                return
+        print(cakename,"재고가 없습니다.")
+
 
     def print_current_money(self):
+        print("현재 판매 금액은 총",self.money,'입니다.')
         pass
 
 
--- Q2.py
+++ Q2.py
@@ -1,37 +1,49 @@
 '''
-시작 시각 :
-종료 시각 :
+시작 시각 :16시
+종료 시각 :16시 40분
 
-
+참고 링크:https://coding-groot.tistory.com/103
 다음 코드에서, 출력되는 결과값이 왜 실제 출력되어야 하는 결과값보다 작은지 설명하시오.
+-> 아래 코드에서 같은 변수를 동시에 접근했기 때문이다. 여러개의 thread가 동시다발적으로 worker 객체에 접근하면
+    마찬가지로 count함수에 동시에 여러 thread들이 작업을 진행할 것이므로, count가 순차적으로 값이 갱신되야 하는데
+    오류가 생길 것이다.
 
 해당 문제점을 해결하기 위해 사용해야 하는 해결책을 서술하고,
+-> Lock을 걸어서 만약 하나의 thread가 변수를 사용하고 있으면 다른 쪽은 사용 못하도록 해야한다.
+   Lock.acquire(): 다른 thread가 접근 못하도록 잠금
+   Lock.release(): 잠금해제
 해당 개념을 자율차 코드에 적용하였을 때
 1) 어느 부분에 적용할 수 있을지,
+-> 자율차에는 cam 4개 및 각종 센서들 (lidar, imu, gps 등등),platform 이 동시에 작업을 해야 하므로 thread를 이용하고있다.
+   따라서 만약 이러한 센서값들에서 얻은 data를 통해 Database class나 controldata.py에 있는 data들을 얻는 과정에서
+   다음과같은 multithreading으로 인한 오류가 발생하면 그러한 부분에서 적용할 수 있을 것 같다.
 2) 이를 통해 어떤 이점을 얻을 수 있는지
 서술하시오.
-
+-> 더 정확한 값을 얻을수 있을 것이다. (하지만, 오버헤드가 생기는 단점이 있있.)
 (Optional) 아래 코드의 실행 결과값이 실제 출력되어야 하는 결과값과 동일하게 출력되도록 변경하시오.
-
+-> 맞게 한거 같은데 왜 안될까? ㅠㅠ
 *** Write Your Answer Below ***
-
+위에다가 각각 적었습니다.
 
 *** Your Answer Ends Here ***
 '''
 
-
+from threading import Lock
 from threading import Thread
 
-
+lock=Lock()
 class Count:
     def __init__(self):
         self.count = 0
 
     def add_offset(self, offset):
+        lock.acquire()
         self.count += offset
+        print("count")
+        lock.release()
 
 
-def worker(idx, limit, count_obj):
+def worker(idx, limit, count_obj): ## idx limit=1000000 ,count_obj
     print(idx)
     for _ in range(limit):
         count_obj.add_offset(1)
